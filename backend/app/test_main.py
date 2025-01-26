4dcf4926315f59382e4f62fcfbdd34be3d9e6050
Ivan Ivashkovskii <ivan290895@gmail.com>
2025-01-26 21:26:06 +0200

    Update test_main.py

--- backend/app/test_main.py
+++ backend/app/test_main.py
@@ -4,7 +4,7 @@ from .main import app
 client = TestClient(app)
 
 
-def  test_read_item():
+def test_read_item():
     response = client.get("/items/5?q=abc")
     assert response.status_code == 200
     assert response.json() == {"item_id": 5, "q": "abc"}
