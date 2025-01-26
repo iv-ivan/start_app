776e632f6b46d3df89ec5119da013bde8f5c055c
Ivan Ivashkovskii <ivan290895@gmail.com>
2025-01-26 21:20:07 +0200

    fix

--- backend/app/test_main.py
+++ backend/app/test_main.py
@@ -7,4 +7,4 @@ client = TestClient(app)
 def test_read_item():
     response = client.get("/items/5?q=abc")
     assert response.status_code == 200
-    assert response.json() = {"item_id": 5, "q": "abc"}
+    assert response.json() == {"item_id": 5, "q": "abc"}
